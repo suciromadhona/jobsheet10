ac97c9cd667935d0a9f7867561408002b4bcbcee
suciromadhona <163643795+suciromadhona@users.noreply.github.com>
2024-06-23 15:21:58 +0700

    final commit

--- AndroidBundle/app/src/main/java/com/example/androidbundlenew/MainActivity.kt
+++ AndroidBundle/app/src/main/java/com/example/androidbundlenew/MainActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
--- AndroidBundle/app/src/main/java/com/example/androidbundlenew/MainActivity2.kt
+++ AndroidBundle/app/src/main/java/com/example/androidbundlenew/MainActivity2.kt
@@ -2,10 +2,7 @@ package com.example.androidbundlenew
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
 
